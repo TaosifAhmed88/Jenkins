91dcb5528f7f8499b930f2611fad2478451c26a6
TaosifAhmed88 <138560477+TaosifAhmed88@users.noreply.github.com>
2025-07-30 00:01:47 +0530

    Update main.tf

--- Day-3 Triggers/main.tf
+++ Day-3 Triggers/main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "name" {
   ami = "ami-0f88e80871fd81e91"
-  instance_type = "t2.medium"
+  instance_type = "t3.medium"
   tags = {Name="Instance Created via CICDcd "}
 }
